6c04c5042ec8b4961d1087f39d34b09459ffd827
Xiubo <zhaoxiubo144@gmail.com>
2019-11-06 21:14:26 -0700

    Movable agents

---
+++ Assets/Agent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public List<GameObject> wayPoints;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(agent!=null)
+        {
+            agent.enabled = true;
+            agent.isStopped = false;
+            if (wayPoints[0] != null)
+                agent.SetDestination(wayPoints[0].transform.position);
+        }
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger: " + other.name);
+        if (other.transform.GetComponent<Node>())
+        {
+            
+        }
+    }
+
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> linkedNodes;
+    public Node GetNextNode()
+    {
+        if(linkedNodes[0]!=null)
+        {
+            return linkedNodes[0];
+        }
+        return null;
+    }
+
+
+#if UNITY_EDITOR
+    /// Draws the links between nodes for editor purposes
+    protected virtual void OnDrawGizmos()
+    {
+        if (linkedNodes == null)
+        {
+            return;
+        }
+        int count = linkedNodes.Count;
+        for (int i = 0; i < count; i++)
+        {
+            Node node = linkedNodes[i];
+            if (node != null)
+            {
+                Gizmos.DrawLine(transform.position, node.transform.position);
+            }
+        }
+    }
+#endif
+}
