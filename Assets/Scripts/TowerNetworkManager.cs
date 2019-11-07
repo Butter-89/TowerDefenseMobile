364fae6a213ade5ad6b2030f26c149eef982642f
Joseph Newman <paradoxbuilder@gmail.com>
2019-11-06 18:30:51 -0700

    Created Tower Network Manager

---
+++ Assets/Scripts/TowerNetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class TowerNetworkManager : MonoBehaviour
+{
+    TrackedLinkedList<GameObject> towers = new TrackedLinkedList<GameObject>();
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            towers.Add(child.gameObject);
+        }
+        towers.GoToNext();
+#if UNITY_EDITOR
+        Debug.Log($"Current tower is {towers.CurrentValue}");
+#endif
+    }
+
+    void GoToNext()
+    {
+        towers.GoToNext();
+    }
+
+    void GoToPrevious()
+    {
+        towers.CurrentValue.SetActive(false);
+        towers.GoToPrevious();
+        towers.CurrentValue.SetActive(true);
+    }
+    
+    /// <summary>
+    /// Adds a tower to this network manager. If the added tower's parent is 
+    /// not this network, it will set it to this network.
+    /// </summary>
+    /// <param name="tower">The tower to add to this network.</param>
+    void CreateTower(GameObject tower)
+    {
+        if (tower.transform.parent != transform)
+            tower.transform.parent = transform;
+        towers.Add(tower);
+    }
+}
