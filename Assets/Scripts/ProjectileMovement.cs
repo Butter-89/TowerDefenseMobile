b2e790d2f326d1e873348eb25e203f571a337cf6
Joseph Newman <paradoxbuilder@gmail.com>
2019-11-07 11:32:00 -0700

    Created script to manage moving objects

---
+++ Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    /// <summary>
+    /// The speed at which the projectile flies.
+    /// </summary>
+    public float ProjectileVelocity = 1f;
+
+    /// <summary>
+    /// The maximum lifetime of this projectile.
+    /// </summary>
+    public float MaxLifetime = 15f;
+
+    private float currentLifetime = 0f;
+    public float CurrentLifetime
+    {
+        get => currentLifetime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, ProjectileVelocity * Time.deltaTime, 0, Space.Self);
+        currentLifetime += Time.deltaTime;
+        if (currentLifetime >= MaxLifetime)
+            Destroy(gameObject);
+    }
+}
