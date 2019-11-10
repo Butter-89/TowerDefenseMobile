385ccdbf0caa084e113ec33ae67e72a776e594d5
Xiubo <zhaoxiubo144@gmail.com>
2019-11-09 17:04:02 -0700

    Adding homing projectile feature & testing

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,8 @@ public class Shooter : MonoBehaviour
 {
     public GameObject projectile;
     public Transform shootFrom;
+    public bool auto_shoot = true;
+    public GameObject target;
     /// <summary>
     /// The rate at which a projectile is fired.
     /// </summary>
@@ -12,16 +14,39 @@ public class Shooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Maybe better to use InvokeRepeating() - edited by Xiubo
+        //InvokeRepeating("Shoot", FireRate, FireRate);
         Invoke("Shoot", FireRate);
         shootFrom = shootFrom ?? gameObject.transform;
     }
 
     public void Shoot()
     {
-        var launched = Instantiate(projectile);
-        launched.transform.position = shootFrom.position;
-        launched.transform.rotation = shootFrom.rotation;
-        launched.SetActive(true);
-        Invoke("Shoot", FireRate);
+        if(auto_shoot)
+        {
+            var launched = Instantiate(projectile);
+            launched.transform.position = shootFrom.position;
+            launched.transform.rotation = shootFrom.rotation;
+            launched.SetActive(true);
+            Invoke("Shoot", FireRate);
+        }
+        else
+        {
+            var launched = Instantiate(projectile);
+            launched.GetComponent<ProjectileMovement>().target = this.target;
+            launched.transform.position = shootFrom.position;
+            launched.transform.rotation = shootFrom.rotation;
+            launched.SetActive(true);
+        }
     }
+
+    //private void Update()
+    //{
+    //    //just for debugging purpose
+    //    if(Input.GetKeyDown(KeyCode.Alpha1))
+    //    {
+    //        Debug.Log("Shoot!");
+    //        Shoot();
+    //    }
+    //}
 }
