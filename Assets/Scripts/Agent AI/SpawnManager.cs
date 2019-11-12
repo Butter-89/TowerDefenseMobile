03dc334c5c620dfc8e8f43c32e083dfe39e7dfcf
Xiubo <zhaoxiubo144@gmail.com>
2019-11-12 14:51:22 -0700

    Editable waves

--- Assets/Scripts/Agent AI/SpawnManager.cs
+++ Assets/Scripts/Agent AI/SpawnManager.cs
@@ -6,33 +6,63 @@ using PathCreation.Examples;
 
 public class SpawnManager : MonoBehaviour
 {
-    public PathCreator[] path_list;
+    public List<WaveData> waves;
     public GameObject spawned_agent;
-    public float time_interval;
-    public int num_toSpawn;
-    int current_num;
+    private PathCreator[] path_list;
+    private float time_interval;
+    private int num_toSpawn;
+    private int current_num;
+    private int current_wave;
     private void Start()
     {
+        current_wave = 0;
         current_num = 0;
+        LoadWaveData(current_wave);
+
         InvokeRepeating("Spawn", time_interval, time_interval);
     }
 
     void Spawn()
     {
-        //randomly choose a path starting from the same point
-        int path_number = Random.Range(0, path_list.Length);
-        PathCreator path_chosen = path_list[path_number];
-        //Instantiate an agent at the start point
-        Vector3 init_position = path_chosen.path.GetPoint(0);
-        Quaternion init_rotation = path_chosen.path.GetRotation(0f);
-        GameObject agent = Instantiate(spawned_agent, init_position, init_rotation);
-        //Attach the agent to the path
-        agent.GetComponent<PathFollower>().pathCreator = path_chosen;
-        current_num++;
-        
-        if(current_num>=num_toSpawn)
+        if (current_num >= num_toSpawn)
         {
             CancelInvoke("Spawn");
+            Debug.Log("Cancel invoke");
         }
+        else
+        {
+            //randomly choose a path starting from the same point
+            int path_number = Random.Range(0, path_list.Length);
+            PathCreator path_chosen = path_list[path_number];
+            //Instantiate an agent at the start point
+            Vector3 init_position = path_chosen.path.GetPoint(0);
+            Quaternion init_rotation = path_chosen.path.GetRotation(0f);
+            GameObject agent = Instantiate(spawned_agent, init_position, init_rotation);
+            //Attach the agent to the path
+            agent.GetComponent<PathFollower>().pathCreator = path_chosen;
+            current_num++;
+
+            if(current_num == num_toSpawn)
+            {
+                //Debug.Log("Current num = to spawn num");
+                //Debug.Log(current_wave + 1 + " " + waves.Count);
+                if(current_wave + 1 <= waves.Count - 1)
+                {
+                    //Debug.Log("!?");
+                    current_wave++;
+                    LoadWaveData(current_wave);
+                } 
+            }
+        }
+    }
+
+    void LoadWaveData(int i)
+    {
+        path_list = waves[i].paths;
+        time_interval = waves[i].interval;
+        num_toSpawn = waves[i].qty;
+        //initialize
+        current_num = 0;
+        Debug.Log("Load wave " + i);
     }
 }
---
+++ Assets/Scripts/Agent AI/WaveData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+//[CreateAssetMenu(fileName = "Wave Data")]
+public class WaveData : MonoBehaviour
+{
+    public PathCreator[] paths;
+    public float interval;
+    public int qty;
+}
