4d7226b607e603f6d87084e73550248c3b040303
Joseph Newman <paradoxbuilder@gmail.com>
2019-11-13 21:12:48 -0700

    created script to toggle monetization

---
+++ Assets/Scripts/UI/MoneyButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyButton : MonoBehaviour
+{
+    public GameObject moneyImage;
+
+    public void ToggleMoneyImage()
+    {
+        moneyImage.SetActive(!moneyImage.activeSelf);
+    }
+}
