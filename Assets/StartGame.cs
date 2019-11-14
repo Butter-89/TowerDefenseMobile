daf3bc4e48df2af355eca403a79bb235766860ed
Xiubo <zhaoxiubo144@gmail.com>
2019-11-13 20:00:14 -0700

    Main Menu

---
+++ Assets/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour
+{
+    private Button startButton;
+    void Start()
+    {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(GameStart);
+    }
+
+    private void GameStart()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+}
