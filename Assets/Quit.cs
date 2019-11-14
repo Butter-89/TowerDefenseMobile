db6ec8ae59700a1b067c1f855729dac7b23edf69
Xiubo <zhaoxiubo144@gmail.com>
2019-11-13 19:31:57 -0700

    Exit button testing

---
+++ Assets/Quit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+    private Button quitButton;
+    void Start()
+    {
+        quitButton = GetComponent<Button>();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
